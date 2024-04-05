d60d9d4c37fb2f6457ab96cff719d7f7e513ec58
Gabriel Liberato De Oliveira Silva <146360818+galiba789@users.noreply.github.com>
2024-04-05 16:35:45 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -1,27 +1,31 @@
-function imc(peso, altura) {
-    var result = peso / (altura*altura)
-    result = parseFloat(result.toFixed(1))
-    if (result < 0) {
-       console.log('se ta morto')
-       return result
-    }
-    else if (result < 18.5) {
-       console.log('voce esta desnutrido') 
-       return result
-    }
-    else if (result <= 25) {
-        console.log('voce esta com o peso baixo')
-        return result
-    }
-    else if (result <= 30) {
-        console.log('voce esta com o peso normal')
-        return result
-    }
-    else if (result > 30) {
-        console.log('voce esta acima do peso')
-        return result
+// module.exports = function() {}
+// module.exports = function() {return 50}
+function imc (peso, altura) {
+    if (isNaN(peso) || isNaN(altura)){
+        return("valor invalido, digite seu peso e altura")
+    } else if (peso <= 0 || altura <= 0){
+        return ("valor invalido, digite seu peso e altura")
+    } else if (peso >= 200 || altura >= 3)
+        return ("valor invalido, digite seu peso e altura")
+    else{
+        return Number((peso/(altura*altura)).toFixed(1))
     }
 
 }
+const message = imc => {
+    if (imc < 18.5) {
+        return (`seu imc está abaixo do peso ideal`)
+    } else if (imc >= 18.6 && imc < 24.9) {
+        return (`seu imc está no peso ideal`)
+    } else if (imc > 25 && imc < 29.9){
+        return(`seu imc está um pouco acima do peso ideal`)
+    } else if (imc > 30 && imc <35.9) {
+        return (`seu imc está acima do peso ideal`)
+    } else if (imc > 36 && imc < 40) {
+        return (`seu imc está bastante acima do peso ideal`)
+    }
+}
 
-module.exports = imc
+
+
+module.exports = {imc, message}
--- imc.test.js
+++ imc.test.js
@@ -1,5 +1,46 @@
-const imc = require('./imc')
+const { imc, message } = require("./imc")
 
-test ('teste de imc', function() {
-    expect(imc(50,1.90)).toBe(13.9)
+describe('cenarios provaveis', () => {
+    test('imc deve retornar uma função', function () {
+        expect(imc).toBeInstanceOf(Function)
+    })
+    test('imc (50, 1) = 50', function () {
+        expect(imc(50, 1.0)).toBe(50)
+    })
+    test('imc de (80, 1.75) = 26.1', function () {
+        expect(imc(50, 1.75)).toBe(16.3)
+    })
+})
+
+describe('cenarios improvaveis', () => {
+    test('imc vazio deve retornar "valor invalido, digite seu peso e altura"', function () {
+        expect(imc()).toBe("valor invalido, digite seu peso e altura")
+    })
+    test('imc ("a", "b") deve retornar "valor invalido, digite seu peso e altura"', function () {
+        expect(imc('a', 'b')).toBe("valor invalido, digite seu peso e altura")
+    })
+    test('imc (0, 0) deve retornar "valor invalido, digite seu peso e altura"', function () {
+        expect(imc(0, 0)).toBe("valor invalido, digite seu peso e altura")
+    })
+    test('imc (200, 3) deve retornar "valor invalido, digite seu peso e altura"', function () {
+        expect(imc(200, 3)).toBe("valor invalido, digite seu peso e altura")
+    })
+})
+
+describe('classificação', () => {
+    test(`imc < 18.5 - seu imc está abaixo do peso ideal`, () => {
+        expect(message(18)).toBe(`seu imc está abaixo do peso ideal`)
+    })
+    test(`imc >= 18.5 e 24.9 - seu imc está  no peso ideal`, () => {
+        expect(message(23)).toBe(`seu imc está no peso ideal`)
+    }) 
+    test(`imc >= 25 e 29.9 - imc está um pouco acima do peso ideal`, () => {
+        expect(message(28)).toBe(`seu imc está um pouco acima do peso ideal`)
+    })
+    test(`imc >= 30 e 34.9 - imc esta acima do peso ideal`, () => {
+        expect(message(32)).toBe(`seu imc está acima do peso ideal`)
+    })
+    test(`imc >= 35 e 39.9 - imc esta bastante acima do peso ideal`, () => {
+        expect(message(38)).toBe(`seu imc está bastante acima do peso ideal`)
+    })
 })
